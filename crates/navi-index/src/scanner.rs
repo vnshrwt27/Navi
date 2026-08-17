9fe00aaab29106d58420eaecda6d6965ec7758b8
Vansh Rawat <vanshrawat643@gmail.com>
2026-08-17 12:50:31 +0000

    created unit tests for the scanner
    created unit test for python and unknown file
    improved comments
    fixed some logical issues

--- crates/navi-index/src/scanner.rs
+++ crates/navi-index/src/scanner.rs
@@ -5,9 +5,11 @@ pub struct ScannedFile {
     pub language: Language
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Language {
     Python,
+ //   Rust,
+//  Javascript,
     Unknown,
 }
 
@@ -15,7 +17,8 @@ pub struct Scanner;
 
 impl Scanner{
     pub fn scan(root: &Path)-> Vec<ScannedFile>{
-        //scans the root directory
+        // scans the project root directory where the tool is initialized 
+        
         let mut files = Vec::new();
 
         Self::walk(root, &mut files);
@@ -27,31 +30,67 @@ impl Scanner{
         let Ok(entries) =  std::fs::read_dir(path) else {
             return ;
         };
-        // Add each file to the files list 
-        // recusively call walk function for all directories
 
-         for entry in entries.flatten(){
-            let path = entry.path();
-            
+         for entry in entries{
+             let Ok(entry) = entry else {
+                 // skip any files that give error 
+                 continue;
+             };
+
+             let path = entry.path();
+
+            // recursively scan sub directories 
             if path.is_dir(){
                 Self::walk(&path, files);
                 continue;
             }
-            if path.is_file(){
+
+            // skip everything that isn't a regular file 
+            if !path.is_file(){
                 continue;
             }
 
             let language = Self::detect_language(&path);
-            files.push(ScannedFile{path,language});
+            files.push(ScannedFile{
+                path,
+                language});
         }
     }
 
     fn detect_language(path: &Path)-> Language{
-        //detect Language by matchin the file type to the language 
+        //detect Language by matching the file type to the language 
         match path.extension().and_then(|ext| ext.to_str()) {
             Some("py") => Language::Python,
+            // Some("rs") => Language::Rust,
+            // Some("js") => Language::Javascript,
             _=> Language::Unknown,
         }
 
+    }
+}
+
+// Unit Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn python_file_detection() {
+        // PYTHON test 
+        let path = Path::new("main.py");
+        assert_eq!(
+                Scanner::detect_language(path),
+                Language::Python
+            );
+}
+
+    #[test]
+    fn unknown_file_detection() {
+        let path = Path::new("Readme.md");
+        // UNKNOWN test
+        assert_eq!(
+            Scanner::detect_language(path),
+            Language::Unknown
+            );
     }
 }
